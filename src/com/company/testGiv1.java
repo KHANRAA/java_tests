f423b97f800bd64c0fbf6f56582aa45a5ce802d8
KHANRAA <khanraakash@gmail.com>
2018-04-20 04:55:04 +0530

    initial Test

---
+++ src/com/company/testGiv1.java
@@ -0,0 +1,19 @@
+package com.company;
+
+class Students{
+int roll;
+char c;
+float marks;
+public Students(int roll,char c,float marks){
+    this.roll=roll;
+    this.c=c;
+    this.marks=marks;
+}
+    public String toString()
+    {
+        return "s="+s+";i="+i+";d="+d;
+    }
+
+
+
+}
