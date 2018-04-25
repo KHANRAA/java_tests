0eaf257438fab3e4591b993f0d3677429d7cff21
KHANRAA <khanraakash@gmail.com>
2018-04-25 14:17:46 +0530

    fix

--- src/com/company/testCollection2.java
+++ src/com/company/testCollection2.java
@@ -5,15 +5,14 @@ public class testCollection2 {
         ArrayList<String> al = new ArrayList<String>();
         al.add("Amit");
         al.add("Vijoy");
-        al.add(1, "Sachin");
+        al.add("Sachin");
+        al.add(1,"Akash");
         System.out.println("Element at 2nd position:" + al.get(2));
         ListIterator<String> itr = al.listIterator();
         System.out.println("Traversing elemnts in forward direction...");
         while (itr.hasNext()) {
             System.out.println(itr.next());
         }
-        System.out.println(itr.next());
-
         System.out.println("Traversing elemets in backward direction...");
         while (itr.hasPrevious()) {
             System.out.println(itr.previous());
