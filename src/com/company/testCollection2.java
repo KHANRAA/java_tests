0a17f99161f17ca1703ca615a25f6ed062084f11
KHANRAA <khanraakash@gmail.com>
2018-04-25 14:10:32 +0530

    added programmes

---
+++ src/com/company/testCollection2.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.*;
+public class testCollection2 {
+    public static void main(String args[]) {
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("Amit");
+        al.add("Vijoy");
+        al.add(1, "Sachin");
+        System.out.println("Element at 2nd position:" + al.get(2));
+        ListIterator<String> itr = al.listIterator();
+        System.out.println("Traversing elemnts in forward direction...");
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+        System.out.println(itr.next());
+
+        System.out.println("Traversing elemets in backward direction...");
+        while (itr.hasPrevious()) {
+            System.out.println(itr.previous());
+        }
+    }
+}
